ab38a5e43a233ca620574deb0e0a0d559ed17c35
bhandarisaurav <saurav.bhandari012@gmail.com>
2023-03-10 21:00:01 -0500

    updated the heading in index file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
@@ -15,20 +15,23 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Online Auction Platform</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          The Online Auction Platform is a web app for safe online trading. It
+          offers various auctions, automatic bidding, payment and shipping
+          integration, and a mobile app. Built on Laravel and React. See the
+          Readme.md for setup and licensing details.
         </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
